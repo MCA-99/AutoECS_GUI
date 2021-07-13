e3ed5e19cd18335a3a684e8586cc4a6e39fed8d0
MCA-99 <capdet2@gmail.com>
2021-07-13 21:28:29 +0200

    todo desde 0

--- AutoECS.py
+++ AutoECS.py
@@ -1,16 +1,43 @@
-# Importes
+"""
+Import tkinter
+@mca 13/07/2021
+"""
 import os
 import tkinter as tk
 from tkinter import *
 
-# Creamos la intancia de la ventana principal y la configuramos
+"""
+This section create the instance of the main window and configure it
+@mca 13/07/2021
+"""
 root = tk.Tk()
 root.title("AutoECS")
 root.tk.call('wm', 'iconphoto', root._w, PhotoImage(file='favicon.png'))
 root.geometry("1280x720")
-root.resizable(False, False)
+root.resizable(0,0)
 
-# Otuput de los comandos ejecutados
+"""
+This section create the frames where the elements will go
+@mca 13/07/2021
+"""
+# menu frame
+menu_frame = Frame(root)
+menu_frame.pack(side=LEFT, padx=20, pady=20, fill="both")
+menu_frame.config(width="500", bg="white")
+# submenu frame
+submenu_frame = Frame(menu_frame)
+submenu_frame.pack(side=LEFT, padx=20, pady=20, fill="both")
+submenu_frame.config(width="10", bg="red")
+# log frame
+log_frame = Frame(root)
+log_frame.pack(side=RIGHT, padx=20, pady=20, fill="both")
+log_frame.config(width="650", bg="white")
+
+"""
+This section creates two text widgets to simulate the output of a terminal
+@mca 13/07/2021
+"""
+# ASCII art
 title = r"""
  ██       ██████   ██████  
  ██      ██    ██ ██       
@@ -19,41 +46,92 @@ title = r"""
  ███████  ██████   ██████
 
 ██████████████████████████████████████████████████████████████████████████████████████████"""
-output_title = Text(root, bg="white", bd=0, highlightthickness = 0, borderwidth=0, state=NORMAL)
+# Create the output_title text widget and configure
+output_title = Text(log_frame, bg="white", bd=0, highlightthickness=0, borderwidth=0, state=NORMAL)
 output_title.insert(END, title)
 output_title.config(state=DISABLED)
 output_title.config(width=90, height=8)
-output_title.place(x=550, y=9)
+output_title.place(x=0, y=0)
+# Help text
+help = r"""
+Este programa simplifica el proceso de BurnIn en los equipos fabricados:
 
-output = Text(root, bg="white", bd=0, highlightthickness = 0, borderwidth=0, state=DISABLED, yscrollcommand=TRUE)
-output.config(width=90, height=33)
-output.place(x=550, y=148)
+»El modo automático pasará por todas las acciones hasta realizar todo el
+proceso de forma automatica.
+»El menú de acciones permite al usuario realizar una acción especifica.
 
 
-# Definimos los colores para los outputs afirmativos/negativos/neutro
+
+
+Segun el color en los de las opciones significa una cosa u otra:
+
+»Azul: La opcion se puede utilizar con normalidad, no requiere permisos de
+      administrador.
+
+»Naranja: La opcion requiere permisos de administrador, si no se ha ininiciado
+         el programa como administrador es necesario reiniciarlo y abrirlo con
+         los permisos correspondientes.
+
+»Rojo: Se utiliza para marcar la opcion de volover atras, o en caso de error.
+
+»Morado: Se utiliza para marcar la opcion de ayuda.
+
+
+
+
+
+
+Made by: MCA
+Contact: m.capdet@e-corp.es
+"""
+# Create the output text widget and configure
+output = Text(log_frame, bg="white", bd=0, highlightthickness=0, borderwidth=0, state=NORMAL, yscrollcommand=TRUE)
+output.insert(END, help)
+output.config(width=90, height=33, state=DISABLED, cursor="pencil")
+output.place(x=10, y=148)
+# Define the colors for the affirmative / negative / neutral outputs
 output.tag_config('success', foreground="green")
 output.tag_config('fail', foreground="red")
 output.tag_config('warning', foreground="orange")
 output.tag_config('neutral', foreground="purple")
 
-# Modo automatico
-def jaja():
+"""
+This section create a button to call a function that goes through all actions automatically
+@mca 13/07/2021
+"""
+# This function goes through all actions automatically
+def auto():
     return 0
+# Create a button to call the auto mode function
+auto_button = Button(menu_frame, text="Modo Automático", command=auto)
+auto_button.pack(padx=50, pady=25)
+auto_button.config(width=53, height=5, cursor="gumby")
 
-auto_button = Button(text="Auto", command=jaja)
-auto_button.place(x=50, y=50)
-
-
-# Acciones
-def menu_acciones():
+"""
+This section create a button to call a function that goes through all actions automatically
+@mca 13/07/2021
+"""
+# This function shows all the actions that the user can perform
+def actions_menu():
     return 0
+# Create a button to call the actions menu function
+actions_menu_button = Button(menu_frame, text="Acciones", command=actions_menu)
+actions_menu_button.pack(padx=50, pady=25)
+actions_menu_button.config(width=53, height=5, cursor="spider")
 
-auto_button = Button(text="Acciones", command=menu_acciones)
-auto_button.place(x=50, y=80)
+"""
+This section create a button to exit the program
+@mca 13/07/2021
+"""
+# Create a button to exit the program
+exit_button = Button(menu_frame, text="Salir", command=root.destroy)
+exit_button.pack(side=BOTTOM, padx=50, pady=25)
+exit_button.config(width=53, height=5, cursor="X_cursor")
 
-# Salir
-auto_button = Button(text="Salir", command=root.destroy)
-auto_button.place(x=50, y=680)
+
+
+
+
 
 
 
@@ -72,14 +150,12 @@ def jaja():
     else:
         output.insert(END, "FRACASO TOTAL\n", "fail")
     output.config(state=DISABLED)
-
-a_button = Button(text="test", command=jaja)
+a_button = Button(root, text="test", command=jaja)
 a_button.place(x=1, y=1)
 ################################################################
 
 
 
 
-
-# Lanza la aplicación
+# Launch the app
 root.mainloop()
